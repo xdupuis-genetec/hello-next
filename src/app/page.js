1ad97b6abfeb47691a619c9aa012320f799735c1
XavierDupuis <xdupuis@genetec.com>
2025-07-16 08:14:23 -0400

    hit counter

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+import {
+  readFile,
+  writeFile,
+} from '../helpers/file-helpers';
+
+const DATABASE_PATH = '/src/database.json';
+
+/*
+`readFile` takes 1 argument:
+• the path to the file:
+
+readFile('/path/to/file');
+
+`writeFile` takes 2 arguments:
+• The path to the file
+• The new contents for the file
+
+writeFile(
+  '/path/to/file',
+  '{ "hello": "world" }'
+);
+*/
+
 function Home() {
+  const databaseString = readFile(DATABASE_PATH);
+  let { hits } = JSON.parse(databaseString);
+  hits++;
+  writeFile(
+    DATABASE_PATH,
+    JSON.stringify({ hits })
+  );
+
   return (
     <main>
-      <h1>Hello Next!</h1>
+      <h1>Welcome!</h1>
+      <p>You are visitor number {hits}</p>
     </main>
   );
 }
